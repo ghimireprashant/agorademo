4876a0187715c48104a21039c784961ab66a0e46
Prashant Ghimire <prashant.ghimire@gmail.com>
2021-10-07 18:46:34 +0545

    init

---
+++ AgoraInjectStreaming/KeyCenter.swift
@@ -0,0 +1,11 @@
+//
+//  KeyCenter.swift
+//  AgoraInjectStreaming
+//
+//  Created by ZhangJi on 2018/7/19.
+//  Copyright © 2018 ZhangJi. All rights reserved.
+//
+
+struct KeyCenter {
+    static let AppId: String = "7f529905a2e84d05a6fe44489197ca20"
+}
